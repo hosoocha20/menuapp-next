03178cb99d65330eb18b96b6e47990f8d7601a4a
Hosoo Cha <hosoo.cha@gmail.com>
2024-09-07 23:40:15 +1200

    /features - design phone + tablet option

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -2,15 +2,27 @@
 
 import { Brushf, Controlf, Customizef, Posf } from "@/ui/FeaturesIcon";
 import KioskUi from "@/ui/KioskUi";
-import React from "react";
+import React, { useState } from "react";
 import { SlArrowRight } from "react-icons/sl";
-import { EmblaOptionsType } from 'embla-carousel'
+import { EmblaOptionsType } from "embla-carousel";
 import EmblaCarousel from "@/ui/EmblaCarousel";
 
 const FeaturesPage = () => {
-    const OPTIONS: EmblaOptionsType = { loop: true }
+  const [designThemeTActive, setDesignThemeTActive] = useState(true);
 
-    const SLIDES = ['bg-[url("/tabletmenusvg.svg")]','bg-[url("/designmenu1.svg")]', 'bg-[url("/designmenu2.svg")]', 'bg-[url("/designmenu3.svg")]']
+  const OPTIONS: EmblaOptionsType = { loop: true };
+
+  const SLIDES_D_T = [
+    'bg-[url("/tabletmenusvg.svg")]',
+    'bg-[url("/designmenu1.svg")]',
+    'bg-[url("/designmenu2.svg")]',
+    'bg-[url("/designmenu3.svg")]',
+  ];
+  const SLIDES_D_P = [
+    '/phonemenudesign1.svg',
+    '/phonemenudesign2.svg',
+    '/phonemenudesign3.svg',
+  ];
   return (
     <div>
       <section className="font-inter h-[75vh] sm:min-h-[500px]   px-[1rem] sm:px-[2rem]">
@@ -60,31 +72,73 @@ const FeaturesPage = () => {
         </div>
       </section>
       <section className="px-mob-nav sm:px-sm-nav  tablet:px-tablet-nav lg:px-lg-nav py-[4rem] min-h-[80vh]">
-        <div className="flex flex-col  h-full ">
-            <h2 className="text-[1.5rem] font-semibold py-[2rem]">Customize Your Menu Theme to Reflect Your Restaurant's Brand</h2>
-            <div className="  h-full  flex flex-wrap gap-[2rem] xl:gap-x-[4rem]   justify-center items-center">
-              {/* <KioskUi img='bg-[url("/designmenu1.svg")]'/> */}
-              <div className=" h-full max-w-[700px] w-full ">
-                <EmblaCarousel slides={SLIDES} options={OPTIONS} />
-                  {/* <Carasoul arr={['bg-[url("/tabletmenusvg.svg")]','bg-[url("/designmenu1.svg")]', 'bg-[url("/designmenu2.svg")]', 'bg-[url("/designmenu3.svg")]']}/> */}
-              </div>
-              <div className=" h-full w-full flex-1 min-w-[300px]  max-w-[500px]">
-                <h3 className="text-[1.3rem] font-semibold mb-[2rem] text-my-orange-400">Design Your Menu</h3>
-                <ul className="flex flex-col gap-3 mb-[7rem] ">
-                    <li className="font-semibold text-[0.95rem]">Choose from a variety of designs
-                        <ul>
-                            <li className="font-normal ">Select and customize a design that matches your restaurant's personality.</li>
-                        </ul>
-                    </li>
-                    <li className="font-semibold text-[0.95rem]">Customize menu categories
-                        <ul>
-                            <li className="font-normal ">Organize your menu items into clear categories that match your restaurant's style.</li>
-                        </ul>
-                    </li>
-                </ul>
+        <div className="flex flex-col  h-full gap-[2rem]">
+          <h2 className="text-[1.5rem] font-semibold  text-center">
+            Customize Your Menu Theme to Reflect Your Restaurant's Brand
+          </h2>
+          <div className="flex justify-center  *:py-3  *:px-6 *:tracking-wide *:font-medium">
+            <button
+              className={`border border-my-orange-400 ${
+                designThemeTActive
+                  ? "bg-my-orange-400 text-white"
+                  : "bg-white text-my-orange-400"
+              }`}
+              onClick={() => setDesignThemeTActive(true)}
+            >
+              Tablet Theme
+            </button>
+            <button
+              className={`border border-my-orange-400 border-l-0 ${
+                designThemeTActive
+                  ? "bg-white text-my-orange-400"
+                  : "bg-my-orange-400 text-white"
+              }`}
+              onClick={() => setDesignThemeTActive(false)}
+            >
+              Phone Theme
+            </button>
+          </div>
+          <div className="  h-full  flex flex-wrap gap-[2rem] xl:gap-x-[4rem]   justify-center items-center">
+            {/* <KioskUi img='bg-[url("/designmenu1.svg")]'/> */}
+
+            {designThemeTActive ? (
+              <div className=" h-full min-w-[300px] max-w-[700px] ">
+                <EmblaCarousel slides={SLIDES_D_T} options={OPTIONS} activeT={true}/>
 
               </div>
+            ) : (
+              <div className=" h-full  w-full max-w-[700px] ">
+                <EmblaCarousel slides={SLIDES_D_P} options={OPTIONS} activeT={false}/>
+
+              </div>
+            )}
+
+            <div className=" h-full w-full flex-1 min-w-[300px]  max-w-[500px] ">
+              <h3 className="text-[1.3rem] font-semibold mb-[2rem] text-my-orange-400">
+                Design Your Menu
+              </h3>
+              <ul className="flex flex-col gap-3 mb-[7rem] ">
+                <li className="font-semibold text-[0.95rem]">
+                  Choose from a variety of designs
+                  <ul>
+                    <li className="font-normal ">
+                      Select and customize a design that matches your
+                      restaurant's personality.
+                    </li>
+                  </ul>
+                </li>
+                <li className="font-semibold text-[0.95rem]">
+                  Customize menu categories
+                  <ul>
+                    <li className="font-normal ">
+                      Organize your menu items into clear categories that match
+                      your restaurant's style.
+                    </li>
+                  </ul>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
       </section>
     </div>
--- src/ui/EmblaCarousel.tsx
+++ src/ui/EmblaCarousel.tsx
@@ -1,56 +1,65 @@
-import React, { useState } from 'react'
-import { EmblaOptionsType } from 'embla-carousel'
-import Autoplay from 'embla-carousel-autoplay'
-import useEmblaCarousel from 'embla-carousel-react'
-import { DotButton, useDotButton } from './EmblaCarouselDotButton'
-import KioskUi from './KioskUi'
-import TabletUi from './TabletUi'
+import React, { useState } from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
+import KioskUi from "./KioskUi";
+import TabletUi from "./TabletUi";
 
 type PropType = {
-  slides: string[]
-  options?: EmblaOptionsType
-}
+  slides: string[];
+  options?: EmblaOptionsType;
+  activeT: boolean;
+};
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
-  const { slides, options } = props
+  const { slides, options } = props;
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [
-    Autoplay({ playOnInit: true, delay: 3000, stopOnInteraction: false })
-  ])
-  const [isPlaying, setIsPlaying] = useState(false)
+    Autoplay({ playOnInit: true, delay: 3000, stopOnInteraction: false }),
+  ]);
+  const [isPlaying, setIsPlaying] = useState(false);
   const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi)
-
-
+    useDotButton(emblaApi);
 
   return (
     <section className="embla w-full h-full  ">
       <div className="embla__viewport w-full h-full " ref={emblaRef}>
         <div className="embla__container w-full h-full ">
           {slides.map((img: string, index) => (
-            <div className="embla__slide  w-full" key={index}>
-              <TabletUi img={img} />
+            <div className="embla__slide  w-full " key={index}>
+     
+              {props.activeT ? (
+                <TabletUi img={img} />
+              ) : (
+                <div className="max-w-[300px]    m-auto">
+                  <img
+                    src={img}
+                    alt="Phone Menu Designs Image"
+                    className="w-full h-[450px] sm:h-[500px] md:h-full "
+                  />
+                </div>
+                
+              )}
             </div>
           ))}
         </div>
       </div>
 
       <div className="embla__controls  z-[10]">
-
-
         <div className="embla__dots  justify-center">
           {scrollSnaps.map((_, index) => (
             <DotButton
               key={index}
               onClick={() => onDotButtonClick(index)}
-              className={'embla__dot'.concat(
-                index === selectedIndex ? ' embla__dot--selected' : ''
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot--selected" : ""
               )}
             />
           ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default EmblaCarousel
+export default EmblaCarousel;
