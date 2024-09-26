79e551998accdf4eeda710ece3ec10dd5ce83677
Hosoo Cha <hosoo.cha@gmail.com>
2024-09-26 22:32:03 +1200

    /products -- first hero - responsive

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,11 +8,19 @@ import { IoMailOutline } from "react-icons/io5";
 const ProductsPage = () => {
   return (
     <div>
-      <section className="font-inter flex  justify-between h-[100vh] sm:min-h-[500px]   px-mob-nav sm:px-sm-nav  tablet:px-tablet-nav lg:px-lg-nav py-[2rem] lg:py-[4rem] ">
-        <div className="flex flex-col gap-[2.5rem] mt-[2rem]">
-          <h1 className="text-[3rem] font-semibold text-my-black-950 leading-tight">
+      <section className="border font-inter flex flex-col tablet:flex-row-reverse gap-x-[2rem] tablet:justify-between h-[100vh]  px-mob-nav sm:px-sm-nav  tablet:px-tablet-nav lg:px-lg-nav py-[2rem] lg:py-[4rem] ">
+
+        <div className=" w-full px-3 tablet:px-0 ">
+          <img
+            src="/phoneproduct2.svg"
+            alt="Image of Phone Menu in Products"
+            className="mx-auto my-0 tablet:ml-auto tablet:mr-0  w-full max-w-[500px] lg:min-w-[500px] xl:max-w-[700px]  "
+          />
+        </div>
+        <div className="flex flex-col gap-[2.5rem] mt-[-2rem] tablet:mt-[2rem]">
+          <h1 className="text-[1.9rem] tablet:text-[2.5rem] xl:text-[3rem] font-semibold text-my-black-950 leading-tight">
             Are you ready{" "}
-            <span className="text-my-purple-500 inline-block align-middle">
+            <span className="text-my-purple-500 inline-block align-middle whitespace-nowrap">
               <GiDiamonds />
             </span>
             <span className="block max-w-fit">to take your restaurant</span>
@@ -21,7 +29,7 @@ const ProductsPage = () => {
               next level?
             </span>
           </h1>
-          <p className="w-[600px]">
+          <p className="xl:w-[600px]">
             The Future of Restaurant Management is Here{" "}
             <span className="inline-block text-my-black-900">
               <FaArrowTrendUp />
@@ -32,21 +40,16 @@ const ProductsPage = () => {
             </span>
             <span>Act Now and Start Seeing Results.</span>
           </p>
-          <form className="border-2 border-my-black-100 rounded-[2rem] pl-[4rem] pr-[200px] flex relative py-[0.5rem] tablet:py-[0.65rem]">
+          <form className="w-full md:w-[450px] tablet:w-auto xl:w-[550px] border-2 border-my-black-100 rounded-[2rem] pl-[4rem] pr-[200px] flex relative py-[0.5rem] tablet:py-[0.65rem]">
             <input placeholder="Your email address" className="w-full pr-2 focus:outline-none"/>
             <IoMailOutline  className="absolute left-[1.5rem] top-[50%] translate-y-[-50%] text-[1.4rem] flex text-my-black-500"/>
             <button
-              className={`absolute right-0 top-0   tablet:bg-my-black-950 tablet:text-white  rounded-[2rem] px-5 py-[0.5rem] tablet:py-[0.65rem] w-[200px] `}
+              className={`absolute right-[-1px] top-[-2px]  bg-my-black-950 text-white  rounded-[2rem] px-5 py-[0.6rem] tablet:py-[0.75rem] w-[200px] `}
             >
               Get Started
             </button>
           </form>
         </div>
-        <img
-          src="/phoneproduct2.svg"
-          alt="Image of Phone Menu in Products"
-          className="max-h-[500px] lg:h-[700px] lg:max-h-[700px]"
-        />
       </section>
     </div>
   );
