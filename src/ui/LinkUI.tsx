845e6eba11fef0fec592b72d0607edad75009f81
Hosoo Cha <hosoo.cha@gmail.com>
2024-09-28 23:16:02 +1200

    customizable props

--- src/ui/LinkUI.tsx
+++ src/ui/LinkUI.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
  interface LinkUIProps{
     name: string
+    color?: string
+    underlineColor?: string
  }
 const LinkUI = (props: LinkUIProps) => {
   return (
     <div className='group w-fit'>
         <a href={`/${props.name.toLowerCase()}`} className='w-min'>
-            <p className='px-[0.2rem] py-1 font-medium text-[0.9rem] lg:text-base'>{props.name}</p>
-            <div className='w-full h-[2px] bg-my-black-200 '>
-                <div className=' scale-x-100 origin-[0_50%] h-full w-full bg-my-black-900 group-hover:scale-x-0 group-hover:origin-[100%_0] transition-transform duration-[250ms] ease-in-out'></div>
+            <p className={`px-[0.2rem] py-1 font-medium text-[0.9rem] lg:text-base`} style={{color: props.color}}>{props.name}</p>
+            <div className='w-full h-[2px] bg-my-black-200 ' style={{backgroundColor: props.underlineColor}}>
+                <div className=' scale-x-100 origin-[0_50%] h-full w-full bg-my-black-900 group-hover:scale-x-0 group-hover:origin-[100%_0] transition-transform duration-[250ms] ease-in-out' style={{backgroundColor: props.color}}></div>
             </div>
         </a>
     </div>
