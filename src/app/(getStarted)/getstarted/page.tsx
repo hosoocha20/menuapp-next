ed11de64043f5886f44f94e7bb138fd507b8ce90
Hosoo Cha <hosoo.cha@gmail.com>
2024-10-12 22:40:06 +1300

    /getstarted page

--- src/app/(getStarted)/getstarted/page.tsx
+++ src/app/(getStarted)/getstarted/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const getStarted = () => {
   return (
-    <div>page</div>
+    <div className='font-inter grid grid-cols-[30%_auto] w-full h-screen'>
+        <div className='h-full bg-my-primary-500 text-white px-8 py-[3rem]'>
+            <p>SmartMenu</p>
+            <div>
+                <h2 className='font-monsterrat text-[2rem] font-medium'>Start creating your digital menu with us.</h2>
+                <h3 className='text-my-primary-200 pr-8'>Discover the worlds best community of freelancers and business people.</h3>
+            </div>
+        </div>
+        <div className='h-full'></div>
+    </div>
   )
 }
 
