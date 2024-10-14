3b9b0ea8628402687ebeae3e11b0db2e967b94c4
Hosoo Cha <hosoo.cha@gmail.com>
2024-10-15 01:10:19 +1300

    /getstarted - form - select pos provider SELECT

--- src/app/(getStarted)/getstarted/page.tsx
+++ src/app/(getStarted)/getstarted/page.tsx
@@ -7,7 +7,9 @@ const getStarted = () => {
   return (
     <div className="font-inter grid lg:grid-cols-[350px_auto] xl:grid-cols-[30%_auto] w-full h-screen ">
       <div className='h-full w-full  bg-my-primary-400 bg-[url("/getstartedbg3.png")] bg-[length:100%_100%] bg-no-repeat  text-white px-4 sm:px-8 py-[1.5rem] md:py-[3rem]'>
-        <a href="/" className="text-[0.8rem] md:text-base">SmartMenu</a>
+        <a href="/" className="text-[0.8rem] md:text-base">
+          SmartMenu
+        </a>
         <div className="mt-[1rem] md:mt-[3rem] lg:mt-[7rem]">
           <h2 className="font-monsterrat text-[1.4rem] lg:text-[2rem] font-medium leading-7 md:leading-9">
             Start creating your digital menu with us.
@@ -21,7 +23,12 @@ const getStarted = () => {
       <div className="h-full py-[1.5rem] md:py-[2rem] px-[1rem] sm:px-8 md:px-[4rem] xl:px-[10%] relative">
         <div className="absolute right-[1rem] md:right-[10%] flex gap-3 text-[0.84rem] md:text-[0.9rem]">
           <p>Already have an account?</p>
-          <a className="font-medium text-my-accent-500 flex">Login to our portal <span className=""><IoIosArrowRoundForward className="flex text-[1.3rem]"/></span></a>
+          <a className="font-medium text-my-accent-500 flex">
+            Login to our portal{" "}
+            <span className="">
+              <IoIosArrowRoundForward className="flex text-[1.3rem]" />
+            </span>
+          </a>
         </div>
         <div>
           <h1 className="mt-[2.5rem] md:mt-[2rem] text-[1.4rem] md:text-[1.7rem] font-monsterrat font-medium text-my-black-900 tracking-tight">
@@ -110,11 +117,13 @@ const getStarted = () => {
               </label>
               <label className="GETSTARTED-LABEL">
                 Select your POS provider
-                <input
-                  className="GETSTARTED-INPUT focus:outline-none"
-                  type="text"
-                  placeholder="Pos"
-                />
+                <select required className="GETSTARTED-INPUT focus:outline-none" name="pos" id="getstarted-pos">
+                    <option value="" disabled selected hidden>Select current POS provider</option>
+                  <option value="lightspeed">Lightspeed</option>
+                  <option value="dPos">DPos</option>
+                  <option value="sqaure">Sqaure</option>
+                  <option value="swiftpos">SwiftPos</option>
+                </select>
               </label>
               <label className="GETSTARTED-LABEL">
                 Password
